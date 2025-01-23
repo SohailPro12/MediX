3861ac6d632d957cf06fee1d4817fc1d19574f29
noureddine <noureddinezzyn@gmail.com>
2025-01-23 23:32:49 +0100

    test

--- frontend/App.js
+++ frontend/App.js
@@ -3,17 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    <View>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello world!</Text>
+        
       <StatusBar style="auto" />
-    </View>hihiu
+    </View>
+     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
