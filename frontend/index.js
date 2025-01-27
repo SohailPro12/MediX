ddcc46b7185ae2cdcca77e7923d6507da7dbcc83
yassine-fanidi <yassinefanidi55@gmail.com>
2025-01-27 22:19:13 +0100

    I added loginScrenn

--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,7 @@
 import { registerRootComponent } from 'expo';
-<<<<<<< HEAD
 
 import App2 from './LoginFront/App2';
 
-=======
-import App from './App';
->>>>>>> 95620c8d70890ff1132e5a6e2468344ef22aad33
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
