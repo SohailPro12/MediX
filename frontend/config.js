97feb341a7fa8c1a91bc5c0daec1b8a2ffa7e01c
Anassbouderhem <as.bouderhem@gmail.com>
2025-04-17 17:38:58 +0200

    dashboard doctor screen navigation

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://67bc-41-250-105-232.ngrok-free.app"; // Remplacez par l'URL de l'API ngrok
+export const API_URL = "https://9c14-41-251-174-249.ngrok-free.app"; // Remplacez par l'URL de l'API ngrok
