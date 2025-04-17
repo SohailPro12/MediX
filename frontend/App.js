d0c6998e8551fde499a3daa02e6b94287ff61804
EZ-ZYN Noureddine <noureddinezzyn@gmail.com>
2025-04-17 16:17:29 +0100

    test test

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-import AppNavigatorDoctor from "./navigation/AppNavigatorDoctor";
 
-export default function App() {
-  return (
-    <AppNavigatorDoctor/>
-  );
-}
-=======
 import React, { useState } from 'react';
 import AppNavigator from './navigation/AppNavigator';
 import Splash from './screens/LoginFront/SplashScreen'; // Import du Splash
--- frontend/navigation/AppNavigator.js
+++ frontend/navigation/AppNavigator.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from '../screens/LoginFront/LoginScreen';
 import ForgotScreen from '../screens/LoginFront/ForgotScreen';
-import CodeScreen from '../screens/LoginFront/CodeScreen';
 import ResetScreen from "../screens/LoginFront/ResetScreen";
 import HomeScreen from '../screens/HomeScreen'; 
 import CodeSSOScreen from "../screens/LoginFront/CodeSSOScreen";
@@ -20,6 +19,25 @@ import AddDoctor from "../screens/AddDoctor";
 import DoctorList from "../screens/DoctorList";
 import DoctorProfile from "../screens/DoctorProfile";
 
+      {/*Doctor Imports*/}
+      
+import Chat from "../screens/Doctor_Interface/Chat";
+import NextRdv from "../screens/Doctor_Interface/NextRdv";
+import AppointmentsList from "../screens/Doctor_Interface/AppointmentsList";
+import PatientSuivi from "../screens/Doctor_Interface/PatientSuivi";
+import Messagerie from "../screens/Doctor_Interface/Messagerie";
+import AppointmentCalendar from "../screens/Doctor_Interface/AppointmentCalendar";
+import DashboardDoctor from "../screens/Doctor_Interface/DashboardDoctor";
+import AjouterPa from "../screens/Doctor_Interface/AjouterPa";
+import SettingsDScreen from "../screens/Doctor_Interface/SettingsDScreen";
+import DossiersM from "../screens/Doctor_Interface/DossiersM";
+import PatientList from "../screens/Doctor_Interface/PatientList";
+import PrescriptionScreen from "../screens/Doctor_Interface/PrescriptionScreen";
+import ReportScreen from "../screens/Doctor_Interface/ReportScreen";
+import EditDoctorProfile from "../screens/Doctor_Interface/EditDoctorProfile";
+import Ordonnance from "../screens/Doctor_Interface/Ordonnance";
+
+
 
 
 const Stack = createStackNavigator();
@@ -36,7 +54,6 @@ export default function AppNavigator() {
         <Stack.Screen name="RoleScreen" component={RoleScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="ForgotScreen" component={ForgotScreen} />
-        <Stack.Screen name="CodeScreen" component={CodeScreen} />
         <Stack.Screen name="ResetScreen" component={ResetScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="AdminGeneralScreen" component={AdminGeneralScreen} />
@@ -45,6 +62,24 @@ export default function AppNavigator() {
         <Stack.Screen name="AddDoctor" component={AddDoctor} />
         <Stack.Screen name="DoctorList" component={DoctorList} />
         <Stack.Screen name="DoctorProfile" component={DoctorProfile} />
+
+
+                  {/*Doctor Screen*/}
+            <Stack.Screen name="DashboardDoctor" component={DashboardDoctor} />
+            <Stack.Screen name="Chat" component={Chat} />
+            <Stack.Screen name="NextRdv" component={NextRdv} />
+                    <Stack.Screen name="AppointmentsList" component={AppointmentsList} />
+                    <Stack.Screen name="PatientSuivi" component={PatientSuivi} />
+                    <Stack.Screen name="Messagerie" component={Messagerie} />
+                    <Stack.Screen name="AppointmentCalendar" component={AppointmentCalendar} />
+                    <Stack.Screen name="AjouterPa" component={AjouterPa}/>
+                    <Stack.Screen name="DossiersM" component={DossiersM}/>
+                    <Stack.Screen name="PatientList" component={PatientList}/>
+                    <Stack.Screen name="PrescriptionScreen" component={PrescriptionScreen}/>
+                    <Stack.Screen name="ReportScreen" component={ReportScreen}/>
+                    <Stack.Screen name='EditDoctorProfile' component={EditDoctorProfile}/>
+                    <Stack.Screen name='SettingsDScreen' component={SettingsDScreen}/>
+                    <Stack.Screen name='Ordonnance' component={Ordonnance}/>
       </Stack.Navigator>
     </NavigationContainer>
     </I18nextProvider>
