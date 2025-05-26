4c60f0afc96a04fbfb1297f17b7fb3b525302797
Anassbouderhem <as.bouderhem@gmail.com>
2025-05-26 16:07:27 +0200

    Patient v final

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://a5c2-105-156-31-144.ngrok-free.app"; // Remplacez par l'URL de l'API ngrok
+export const API_URL = "https://eafb-41-250-193-136.ngrok-free.app"; // Remplacez par l'URL de l'API ngrok
